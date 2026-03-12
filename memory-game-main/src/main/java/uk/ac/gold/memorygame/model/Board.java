4d5dc972747206b2aa79a2e09b6f00365bc26ebb
Chris <148207385+Xencil@users.noreply.github.com>
2026-03-12 15:05:30 +0000

    working model implementation

---
+++ memory-game-main/src/main/java/uk/ac/gold/memorygame/model/Board.java
@@ -0,0 +1,84 @@
+package uk.ac.gold.memorygame.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import uk.ac.gold.memorygame.model.exceptions.BoardStateException;
+
+public class Board {
+
+    private final List<Card> cards = new ArrayList<>();
+    private int numberOfPairs;
+
+
+    public Board(int numberOfPairs) {
+    	this(numberOfPairs,true);
+    }
+
+    public Board(int numberOfPairs, boolean shuffle) {
+        this.numberOfPairs =numberOfPairs;
+
+        for (int i =0;i <numberOfPairs;i++){
+            cards.add(new Card(i));
+            cards.add(new Card(i));
+        }
+
+        if (shuffle) {
+            Collections.shuffle(cards);
+        }
+    }
+
+    /*
+     * -----------------------------
+     * Access
+     * -----------------------------
+     */
+
+    public List<Card> getCards() {
+        // Prevent modification.
+        return Collections.unmodifiableList(cards);
+    }
+
+    public Card getCard(int id) {
+        return cards.get(id);
+    }
+
+    public int numberOfCards() {
+        return cards.size();
+    }
+
+    public int numberOfPairs() {
+    	return numberOfPairs;
+    }
+
+    /*
+     * -----------------------------
+     * Game-related queries
+     * -----------------------------
+     */
+
+    public int countMatchedCards() {
+    	int counter =0;
+
+        for(Card card : cards){
+            if(card.isMatched()){
+            	counter++;
+            }
+        }
+
+        if (counter % 2 != 0) {
+            throw new BoardStateException("failed");
+        }
+
+        return counter;
+    }
+
+    public int countMatchedPairs() {
+    	return countMatchedCards()/2;
+    }
+
+    public boolean allCardsMatched() {
+    	return countMatchedPairs() ==numberOfPairs;
+    }
+}
---
+++ memory-game-main/src/main/java/uk/ac/gold/memorygame/model/Card.java
@@ -0,0 +1,64 @@
+package uk.ac.gold.memorygame.model;
+
+public class Card {
+
+    private final int pairId; // Unique integer ID for each card pair.
+
+    private boolean faceUp = false;
+    private boolean matched = false;
+
+    public Card(int pairId) {
+        this.pairId = pairId;
+    }
+
+    /*
+     * -----------------------------
+     * Identity & matching
+     * -----------------------------
+     */
+
+    public int getPairId() {
+        return pairId;
+    }
+
+    public boolean matches(Card other) {
+        if(other==null){
+            return false;
+        }
+        return this.pairId ==other.pairId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "%s[pairId=%d]",
+                getClass().getSimpleName(),
+                getPairId());
+    }
+
+    /*
+     * -----------------------------
+     * State
+     * -----------------------------
+     */
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public boolean isMatched() {
+        return matched;
+    }
+
+    public void flipUp() {
+    	faceUp =true;
+    }
+
+    public void flipDown() {
+    	faceUp = false;
+    }
+
+    public void setMatched(boolean matched) {
+    	this.matched =matched;
+    }
+}
